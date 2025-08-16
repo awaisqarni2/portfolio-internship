bc6ffa7bfd2a459995c881a44d68ce6865742178
awaisqarni2 <awais31105@gmail.com>
2025-08-16 23:56:34 +0500

    initial commit

---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="flex w-full justify-between text-white bg-blue-500">
+      <h3>Awais Qarni</h3>
+      <nav className="flex gap-4">
+        <a href="">Home</a>
+        <a href="">Projects</a>
+        <a href="">Resume</a>
+        <a href="">contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
